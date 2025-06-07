d254d58be76e6479c8293fa18cdc1575cadb5586
Edmon <19ed86boy@mail.ru>
2025-06-07 12:50:40 +0300

    init progect

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,19 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs';
-import genDiff from '../src/genDiff.js';
+import { fileURLToPath } from 'url'
+import path from 'path'
+import fs from 'fs'
+import genDiff from '../src/genDiff.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
 
 test('gendiff flat json', () => {
-  const expected = readFile('expected.txt');
+  const expected = readFile('expected.txt')
   const result = genDiff(
     JSON.parse(readFile('file1.json')),
     JSON.parse(readFile('file2.json')),
-  ).trim();
+  ).trim()
 
-  expect(result).toBe(expected);
-});
+  expect(result).toBe(expected)
+})

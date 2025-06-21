da04919ed0f515218e4400fab8f2055240640438
Edmon <19ed86boy@mail.ru>
2025-06-21 18:33:37 +0300

    init progect

--- code/index.js
+++ code/index.js
@@ -1,21 +1,2 @@
-import fs from 'fs'
-import path from 'path'
-import parse from '../src/parsers.js'
-import buildAst from '../src/buildAst.js'
-import format from '../formatters/index.js' // используем универсальный форматер
-
-const getData = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), filepath)
-  const content = fs.readFileSync(absolutePath, 'utf-8')
-  const ext = path.extname(filepath).slice(1)
-  return parse(content, ext)
-}
-
-const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = getData(filepath1)
-  const data2 = getData(filepath2)
-  const ast = buildAst(data1, data2)
-  return format(ast, formatName) // универсальный вызов форматера
-}
-
-export default gendiff
+import gendiff from '../gendiff.js';
+export default gendiff;
